da21be35858ac5eea74bf1bc626fef6163538578
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-07-07 08:27:55 -0700

    Add method to get a transformed bounding box

--- src/Bounds.ts
+++ src/Bounds.ts
@@ -1,4 +1,6 @@
 import * as vec3 from "../external/gl-matrix/vec3.js"
+import * as mat3 from "../external/gl-matrix/mat3.js"
+import * as mat4 from "../external/gl-matrix/mat4.js"
 
 export class Bounds {
     public min = vec3.fromValues(Number.MAX_VALUE, Number.MAX_VALUE, Number.MAX_VALUE);
@@ -14,10 +16,37 @@ export class Bounds {
     }
 
     public center() {
-        let c = vec3.fromValues(0,0,0);
+        let c = vec3.create();
         vec3.add(c, this.min, this.max);
-        vec3.scale(c, c,0.5);
+        vec3.scale(c, c, 0.5);
 
         return c;
+    }
+
+    // Based on code from: https://github.com/erich666/GraphicsGems/blob/master/gems/TransBox.c
+    public static transform(matrix: mat4, bounds: Bounds) {
+        const transform = mat3.create();
+        mat3.fromMat4(transform, matrix);
+        const translate = vec3.fromValues(matrix[12], matrix[13], matrix[14]);
+
+        /* Take care of translation by beginning at T. */
+        const B = Bounds.createFromMinMax(translate, translate);
+
+        /* Now find the extreme points by considering the product of the min and max with each component of M.  */
+        for(let i = 0; i < 3; i++ ) {
+            for (let j = 0; j < 3; j++) {
+                const a = (transform[i * 3 + j] * bounds.min[j]);
+                const b = (transform[i * 3 + j] * bounds.max[j]);
+                if (a < b) {
+                    B.min[i] += a;
+                    B.max[i] += b;
+                } else {
+                    B.min[i] += b;
+                    B.max[i] += a;
+                }
+            }
+        }
+
+        return B;
     }
 }
